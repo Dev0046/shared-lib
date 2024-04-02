4073bb4c5eb8d50af63271ed4ff48f68cd246ebc
dev0046 <devkumargautamg@gmail.com>
2024-04-02 14:07:03 +0530

    new6

--- vars/approval.groovy
+++ vars/approval.groovy
@@ -1,9 +1,16 @@
-// UserApproval.groovy
-def call(String message = 'Approve deployment?', String ok = 'Approve') {
-    try {
-        input message: message, ok: ok
-        return true
-    } catch (Exception e) {
-        return false
+// ApprovalStage.groovy
+
+def call() {
+    stage('Approval') {
+        steps {
+            script {
+                dir('shared-lib') {
+                    def approval = input message: "Approve Deployment?", ok: "Proceed", submitterParameter: "APPROVAL", abort: "Abort"
+                    if (approval == 'Abort') {
+                        error 'Pipeline aborted as deployment was not approved..'
+                    }
+                }
+            }
+        }
     }
 }
