686c2d973fb382422cd8411f018be67f3718ed18
dev0046 <devkumargautamg@gmail.com>
2024-04-02 22:25:58 +0530

    new6

--- vars/playbook.groovy
+++ vars/playbook.groovy
@@ -1,3 +1,3 @@
  def call() {
-    sh 'ansible-playbook -i /var/lib/jenkins/inventory  /var/lib/jenkins/sonar.yaml'
+    sh 'ansible-playbook -i /var/lib/jenkins/workspace/inventory  /var/lib/jenkins/workspace/sonar.yaml'
  }
